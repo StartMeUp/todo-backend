b9d021e080126942d8e9eb1664e7a44679b5651e
StartMeUp <contact@start-me-up.fr>
2021-06-27 20:48:48 +0200

    aws ses credentials working

---
+++ src/emails/index.ts
@@ -0,0 +1,46 @@
+import aws from "aws-sdk";
+
+const params = {
+  Destination: {
+    /* required */
+    CcAddresses: [],
+    ToAddresses: ["chrislep62@gmail.com"],
+  },
+  Message: {
+    /* required */
+    Body: {
+      /* required */
+      Html: {
+        Charset: "UTF-8",
+        Data: "HTML_FORMAT_BODY",
+      },
+      Text: {
+        Charset: "UTF-8",
+        Data: "TEXT_FORMAT_BODY",
+      },
+    },
+    Subject: {
+      Charset: "UTF-8",
+      Data: "Test email",
+    },
+  },
+  Source: "contact@start-me-up.fr" /* required */,
+  ReplyToAddresses: [
+    "contact@start-me-up.fr",
+    /* more items */
+  ],
+};
+
+// Create the promise and SES service object
+const sendPromise = new aws.SES({ apiVersion: "2010-12-01" })
+  .sendEmail(params)
+  .promise();
+
+// Handle promise's fulfilled/rejected states
+sendPromise
+  .then(function (data) {
+    console.log(data.MessageId);
+  })
+  .catch(function (err) {
+    console.error(err, err.stack);
+  });
